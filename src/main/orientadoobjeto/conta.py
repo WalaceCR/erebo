c1d2a9ad6271165c723f97b5d600f87f1faff8d1
WalaceCR <walace.dacunha@yahoo.com.br>
2019-02-27 17:45:56 -0300

    Acessando a classe Conta

--- src/main/orientadoobjeto/conta.py
+++ src/main/orientadoobjeto/conta.py
@@ -1,3 +1,10 @@
 
 class Conta:
-    pass
+
+    ##construtor da classe, aqui dentro crio as variáveis/atributos da classe
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
--- src/main/orientadoobjeto/start.py
+++ src/main/orientadoobjeto/start.py
@@ -1,3 +1,10 @@
 from src.main.orientadoobjeto.conta import Conta
 
-Conta()
+nova_conta = Conta(1, "Luana", 323434545.90, 500000000.90)
+nova_conta2 = Conta(1, "Walace", -900000.90, 500000000.90)
+nova_conta3 = Conta(1, "Luana e Walace", 323434545.90, 500000000.90)
+
+##cada nova execução gera um objeto diferente como exemplo 0x7f5f57f08c50, 0x7f5f57f55555 e etc.
+print("Retornando uma nova conta {} do titular {}".format(nova_conta, nova_conta.titular))
+print("Retornando uma nova conta {} do titular {}".format(nova_conta2, nova_conta2.titular))
+print("Retornando uma nova conta {} do titular {}".format(nova_conta3, nova_conta3.titular))
